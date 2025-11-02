1c3550ba4890bd1a9819c7e6739580b5d228382a
mubarek <mezid58@gmail.com>
2025-11-02 12:08:01 +0300

    file format

--- config/db.js
+++ config/db.js
@@ -1,14 +1,21 @@
 // db.js
 const mongoose = require('mongoose');
-const catchAsync = require('../utils/catchAsync'); // Adjust the path as necessary
 require('dotenv').config();
 
-const connectDB = catchAsync( async () => {
-    await mongoose.connect(process.env.DATABASE_LOCAL, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('✅ MongoDB connected!');
-  } );
+
+    // Load background dispatcher AFTER successful connection
+    require('../utils/dispatcher.js');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1); // Stop the app if the DB connection fails
+  }
+};
 
 module.exports = connectDB;
