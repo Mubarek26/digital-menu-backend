a7c04d5ad5eb82199734193bc5c8d1b827b5bcde
mubarek <mezid58@gmail.com>
2025-11-11 14:17:26 +0300

    uploadImage - Modify uploadImage middleware to serve static files from uploads directory

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,6 +1,8 @@
 const multer = require("multer");
 const AppError = require("../utils/appError");
 const path = require("path");
+const express = require("express");
+const app= express();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
