670fa21864e3168d837a51c2bcade6cb29dfa5b9
Alferid Hassen <nsr-1452-14@amu.edu.et>
2025-12-09 12:32:03 +0300

    commented console logs

--- models/UserModel.js
+++ models/UserModel.js
@@ -97,9 +97,9 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
       10
     );
     
-    console.log('🔍 JWT issued at:', JWTTimestamp);
-    console.log('🔍 Password changed at:', changedTimestamp);
-    console.log('🔍 Changed after?', JWTTimestamp < changedTimestamp);
+    // console.log('🔍 JWT issued at:', JWTTimestamp);
+    // console.log('🔍 Password changed at:', changedTimestamp);
+    // console.log('🔍 Changed after?', JWTTimestamp < changedTimestamp);
     return JWTTimestamp < changedTimestamp; // Return true if password was changed after the token was issued
   }
   return false; // If no passwordChangedAt, return false
@@ -113,7 +113,7 @@ userSchema.methods.createPasswordResetToken = function () {
     .update(resetToken)
     .digest('hex'); // Hash the token before saving it
   this.resetPasswordExpires = Date.now() + 10 * 60 * 1000; // Set expiration time for the token (10 minutes)
-  console.log({ resetToken }, this.resetPasswordToken);
+  // console.log({ resetToken }, this.resetPasswordToken);
   return resetToken;
 };
 
